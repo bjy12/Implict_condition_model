2f0fa8a7c18dff32665c42f8b0e77e4b93d7cbe7
bjy12 <bjywhy@gmail.com>
2024-12-20 16:38:02 +0800

    add sample foward function for block wise sample

--- model/points_wise_diffusion.py
+++ model/points_wise_diffusion.py
@@ -11,6 +11,7 @@ from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
 from diffusers.schedulers.scheduling_ddim import DDIMScheduler
 from diffusers.schedulers.scheduling_pndm import PNDMScheduler
 from torch import Tensor
+from tqdm import tqdm
 
 from model.denoised_model import DenoisedModel
 from model.project_condition_model import ProjectionImplictConditionModel
@@ -50,7 +51,7 @@ class Points_WiseImplict_ConditionDiffusionModel(ProjectionImplictConditionModel
                       coords_idensity: Optional[Tensor] ,  #  ( b , h ,  w ,  d , 3 + 1)  3 is x y z 1 is idensity
                       points_proj: Optional[Tensor] , # (b ,  , n_points , d )
                       xray_projs: Optional[Tensor] ,  # (b , 2 , 1 , u ,v )
-                    return_intermediate_steps: bool = False
+                      return_intermediate_steps: bool = False
         ):
         B  =   coords_idensity.shape[0]
         coords = coords_idensity[:,:,:,:,:3]
@@ -97,13 +98,77 @@ class Points_WiseImplict_ConditionDiffusionModel(ProjectionImplictConditionModel
         coords_idensity = torch.cat([points , idensity]  , dim=-1)
 
         return coords_idensity , proj , points_proj
+    
+    @torch.no_grad()
+    def forward_sample(self, 
+                       coords_idensity: Optional[Tensor] ,  #  ( b , h ,  w ,  d , 3 + 1)  3 is x y z 1 is idensity
+                       points_proj: Optional[Tensor] , # (b ,  , n_points , d )
+                       xray_projs: Optional[Tensor] ,  # (b , 2 , 1 , u ,v )
+                       scheduler: Optional[str] = 'ddpm' ,
+                       num_inference_steps: Optional[int] = 1000,
+                       eta: Optional[float] = 0.0,  # for DDIM
+                       return_sample_every_n_steps: int =  -1 ,
+                       disable_tqdm: bool = False,
+    ):  
+        scheduler = self.scheduler if scheduler is None else self.schedulers_map[scheduler]
+
+        device = self.device
+  
+        b , h , w , d , _ = coords_idensity.shape
+        coords = coords_idensity[...,:3]
+        c = 1  # only predict idensity
+        x_t = torch.randn(b , h , w , d ,c , device=device)
+
+        accepts_offset = "offset" in set(inspect.signature(scheduler.set_timesteps).parameters.keys())
+        extra_set_kwargs = {"offset": 1} if accepts_offset else {}
+        scheduler.set_timesteps(num_inference_steps, **extra_set_kwargs)
+
+        # Prepare extra kwargs for the scheduler step, since not all schedulers have the same signature
+        # eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
+        # eta corresponds to η in DDIM paper: https://arxiv.org/abs/2010.02502
+        # and should be between [0, 1]
+        accepts_eta = "eta" in set(inspect.signature(scheduler.step).parameters.keys())
+        extra_step_kwargs = {"eta": eta} if accepts_eta else {}
+        
+        all_outputs = []
+        return_all_outputs = (return_sample_every_n_steps > 0)
+        progress_bar = tqdm(scheduler.timesteps.to(device), desc=f'Sampling ({x_t.shape})', disable=disable_tqdm)
+        for i , t in enumerate(progress_bar):
+            x_t_input = self.get_input_with_conditioning(x_t ,  xray_projs ,  points_proj , coords )
+
+            noise_pred = self.noised_pred_model(x_t_input,  t.reshape(1).expand(b))
+
+            x_t = scheduler.step(noise_pred, t, x_t, **extra_step_kwargs).prev_sample
+
+            # Append to output list if desired
+            if (return_all_outputs and (i % return_sample_every_n_steps == 0 or i == len(scheduler.timesteps) - 1)):
+                all_outputs.append(x_t)
+
+        x_t = (x_t / 2 + 0.5 ).clamp(0,1)
+        sample_result = x_t.cpu().numpy()
+
+        return sample_result
 
 
 
+            
+
+
+
+
+
+
+
+
+
+        
 
     def forward(self, batch: dict , mode = 'train', **kwargs):
         coords_idensity , proj , points_proj  = self.process_batch(batch)
-        coords_idensity, proj, points_proj = self.process_batch(batch)
+        if mode == 'train':
+            return self.forward_train(coords_idensity , points_proj , proj)
+        elif mode == 'sample':
+            return self.forward_sample(coords_idensity , points_proj , proj)
         # print("Feature stats:", {
         #     "coords_range": (coords_idensity.min(), coords_idensity.max()),
         #     "proj_range": (proj.min(), proj.max()),
@@ -111,7 +176,7 @@ class Points_WiseImplict_ConditionDiffusionModel(ProjectionImplictConditionModel
         # })
 
 
-        return self.forward_train(coords_idensity , points_proj , proj)
+        return 
 
 
          
